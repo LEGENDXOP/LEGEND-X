2a489c73a81871bdce107717e58a3351e1a0b0ef
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-01 17:40:25 +0530

    Update __init__.py

--- LEGEND/__init__.py
+++ LEGEND/__init__.py
@@ -181,7 +181,7 @@ else:
 
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-telethn = TelegramClient("LEGEND", API_ID, API_HASH)
+telethn = TelegramClient("saitama", API_ID, API_HASH)
 pbot = Client("LEGEND", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 
