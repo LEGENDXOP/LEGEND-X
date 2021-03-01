5dacd7c35da82d88cee5c35a14f24b93e12c3f45
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-01 18:03:31 +0530

    Update eval.py

--- LEGEND/modules/eval.py
+++ LEGEND/modules/eval.py
@@ -1,85 +1,137 @@
-import subprocess
-from LEGEND import telethn as bot
-from LEGEND.events import register
-from LEGEND import OWNER_ID, DEV_USERS as SUDO_USERS
-import asyncio
-import traceback
 import io
 import os
-import sys
-import time
-from telethon.tl import functions
-from telethon.tl import types
-from telethon.tl.types import *
-from telethon.errors import *
 
-@register(pattern="/eval")
-async def _(event):
-    if event.sender_id == OWNER_ID:
-        pass
-    elif event.sender_id in SUDO_USERS:
-        pass
+# Common imports for eval
+import textwrap
+import traceback
+from contextlib import redirect_stdout
+
+from telegram import ParseMode, Update
+from telegram.ext import CallbackContext, CommandHandler, run_async
+
+from LEGEND import LOGGER, dispatcher
+from LEGEND.modules.helper_funcs.chat_status import dev_plus
+
+namespaces = {}
+
+
+def namespace_of(chat, update, bot):
+    if chat not in namespaces:
+        namespaces[chat] = {
+            "__builtins__": globals()["__builtins__"],
+            "bot": bot,
+            "effective_message": update.effective_message,
+            "effective_user": update.effective_user,
+            "effective_chat": update.effective_chat,
+            "update": update,
+        }
+
+    return namespaces[chat]
+
+
+def log_input(update):
+    user = update.effective_user.id
+    chat = update.effective_chat.id
+    LOGGER.info(f"IN: {update.effective_message.text} (user={user}, chat={chat})")
+
+
+def send(msg, bot, update):
+    if len(str(msg)) > 2000:
+        with io.BytesIO(str.encode(msg)) as out_file:
+            out_file.name = "output.txt"
+            bot.send_document(chat_id=update.effective_chat.id, document=out_file)
     else:
-        return
-    cmd = event.text.split(" ", maxsplit=1)[1]
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
+        LOGGER.info(f"OUT: '{msg}'")
+        bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f"`{msg}`",
+            parse_mode=ParseMode.MARKDOWN,
+        )
 
-    old_stderr = sys.stderr
-    old_stdout = sys.stdout
-    redirected_output = sys.stdout = io.StringIO()
-    redirected_error = sys.stderr = io.StringIO()
-    stdout, stderr, exc = None, None, None
+
+@dev_plus
+@run_async
+def evaluate(update: Update, context: CallbackContext):
+    bot = context.bot
+    send(do(eval, bot, update), bot, update)
+
+
+@dev_plus
+@run_async
+def execute(update: Update, context: CallbackContext):
+    bot = context.bot
+    send(do(exec, bot, update), bot, update)
+
+
+def cleanup_code(code):
+    if code.startswith("```") and code.endswith("```"):
+        return "\n".join(code.split("\n")[1:-1])
+    return code.strip("` \n")
+
+
+def do(func, bot, update):
+    log_input(update)
+    content = update.message.text.split(" ", 1)[-1]
+    body = cleanup_code(content)
+    env = namespace_of(update.message.chat_id, update, bot)
+
+    os.chdir(os.getcwd())
+    with open(
+        os.path.join(os.getcwd(), "DaisyX/modules/helper_funcs/temp.txt"), "w"
+    ) as temp:
+        temp.write(body)
+
+    stdout = io.StringIO()
+
+    to_compile = f'def func():\n{textwrap.indent(body, "  ")}'
 
     try:
-        await aexec(cmd, event)
+        exec(to_compile, env)
+    except Exception as e:
+        return f"{e.__class__.__name__}: {e}"
+
+    func = env["func"]
+
+    try:
+        with redirect_stdout(stdout):
+            func_return = func()
     except Exception:
-        exc = traceback.format_exc()
-
-    stdout = redirected_output.getvalue()
-    stderr = redirected_error.getvalue()
-    sys.stdout = old_stdout
-    sys.stderr = old_stderr
-
-    evaluation = ""
-    if exc:
-        evaluation = exc
-    elif stderr:
-        evaluation = stderr
-    elif stdout:
-        evaluation = stdout
+        value = stdout.getvalue()
+        return f"{value}{traceback.format_exc()}"
     else:
-        evaluation = "Success"
-
-    final_output = "**EVAL**: `{}` \n\n **OUTPUT**: \n`{}` \n".format(cmd, evaluation)
-    MAX_MESSAGE_SIZE_LIMIT = 4095
-    if len(final_output) > MAX_MESSAGE_SIZE_LIMIT:
-        with io.BytesIO(str.encode(final_output)) as out_file:
-            out_file.name = "eval.text"
-            await bot.send_file(
-                event.chat_id,
-                out_file,
-                force_document=True,
-                allow_cache=False,
-                caption=cmd,
-                reply_to=reply_to_id,
-            )
-
-    else:
-        await event.reply(final_output)
+        value = stdout.getvalue()
+        result = None
+        if func_return is None:
+            if value:
+                result = f"{value}"
+            else:
+                try:
+                    result = f"{repr(eval(body, env))}"
+                except:
+                    pass
+        else:
+            result = f"{value}{func_return}"
+        if result:
+            return result
 
 
-async def aexec(code, smessatatus):
-    message = event = smessatatus
+@dev_plus
+@run_async
+def clear(update: Update, context: CallbackContext):
+    bot = context.bot
+    log_input(update)
+    global namespaces
+    if update.message.chat_id in namespaces:
+        del namespaces[update.message.chat_id]
+    send("Cleared locals.", bot, update)
 
-    def p(_x):
-        return print(slitu.yaml_format(_x))
 
-    reply = await event.get_reply_message()
-    exec(
-        "async def __aexec(message, reply, client, p): "
-        + "\n event = smessatatus = message"
-        + "".join(f"\n {l}" for l in code.split("\n"))
-    )
-    return await locals()["__aexec"](message, reply, bot, p)
+EVAL_HANDLER = CommandHandler(("e", "ev", "eva", "eval"), evaluate)
+EXEC_HANDLER = CommandHandler(("x", "ex", "exe", "exec", "py"), execute)
+CLEAR_HANDLER = CommandHandler("clearlocals", clear)
+
+dispatcher.add_handler(EVAL_HANDLER)
+dispatcher.add_handler(EXEC_HANDLER)
+dispatcher.add_handler(CLEAR_HANDLER)
+
+__mod_name__ = "Eval Module"
