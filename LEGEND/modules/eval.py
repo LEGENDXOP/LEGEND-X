428e8dc4c5a48c3618c8e11d1c34976a117533f9
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-03 11:09:54 +0530

    Update eval.py

--- LEGEND/modules/eval.py
+++ LEGEND/modules/eval.py
@@ -13,6 +13,34 @@ from telethon.tl import types
 from telethon.tl.types import *
 from telethon.errors import *
 
+@register(pattern="^/bash (.*)")
+async def msg(event):
+    if event.sender_id == OWNER_ID or event.sender_id in SUDO_USERS:
+        pass
+    else:
+        return
+    PROCESS_RUN_TIME = 100
+    cmd = event.pattern_match.group(1)
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+    time.time() + PROCESS_RUN_TIME
+    process = await asyncio.create_subprocess_shell(
+        cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+    stdout, stderr = await process.communicate()
+    e = stderr.decode()
+    if not e:
+        e = "No Error"
+    o = stdout.decode()
+    if not o:
+        o = "**Tip**: \n`If you want to see the results of your code, I suggest printing them to stdout.`"
+    else:
+        _o = o.split("\n")
+        o = "`\n".join(_o)
+    await event.reply(f"**QUERY:**\n__Command:__\n`{cmd}` \n__PID:__\n`{process.pid}`\n\n**stderr:** \n`{e}`\n**Output:**\n{o}"
+)
+
 @register(pattern="/eval")
 async def _(event):
     if event.sender_id == OWNER_ID:
