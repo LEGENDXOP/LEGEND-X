ec473f1746215210101332726b219d776305123c
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-07 13:21:00 +0530

    Okay

---
+++ LEGEND/modules/myinfo.py
@@ -0,0 +1,39 @@
+# COPYRIGHT (C) BY LEGENDX22 AND PROBOYX 
+"""
+(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
+(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
+(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
+(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
+               MADE BY LEGENDX22 AND PROBOYX
+              COPYRIGHT BY LEGENDX AND PROBOY
+                 CREDIT #TEAMLEGEND 
+         IF YOU KANG THEN DONT REMOVE THIS LINES 
+"""
+from telethon import custom, events, Button
+import os,re
+from LEGEND import telethn as bot
+from LEGEND import telethn as tgbot
+from LEGEND.events import register 
+@register(pattern="/myinfo")
+async def proboyx(event):
+  button = [[custom.Button.inline("CHECK",data="information")]]
+  await bot.send_message(event.chat, "YOUR INFORMATION",buttons=button)
+
+@tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"information")))
+async def callback_query_handler(event):
+  try:
+    boy = event.sender_id
+    PRO = await bot.get_entity(boy)
+    LEGENDX = "YOUR DETAILS BY GRAND OFFICIAL\n\n"
+    LEGENDX += f"FIRST NAME : {PRO.first_name} \n"
+    LEGENDX += f"LAST NAME : {PRO.last_name}\n"
+    LEGENDX += f"YOU BOT : {PRO.bot} \n"
+    LEGENDX += f"YOU SCAMMER : {PRO.scam}\n"
+    LEGENDX += f"YOUR SUPOORTED : {PRO.support}\n"
+    LEGENDX += f"RESTRICTED : {PRO.restricted} \n"
+    LEGENDX += f"USER ID : {boy}\n"
+    LEGENDX += f"USERNAME : {PRO.username}\n\n\n"
+    LEGENDX += "THANKS FOR USING ME ☺️☺️☺️"
+    await event.answer(LEGENDX, alert=True)
+  except Exception as e:
+    await bot.send_message(proboyx, e)
