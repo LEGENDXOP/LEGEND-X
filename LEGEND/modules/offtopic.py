623b9044e494da3c27f7c52343194078a0106ab2
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-09 17:48:12 +0530

    Create offtopic.py

---
+++ LEGEND/modules/offtopic.py
@@ -0,0 +1,19 @@
+# MADE BY PROBOYX AND LEGENDX22
+
+from telethon import events, Button
+import asyncio
+from LEGENDX import bot as client
+@client.on(events.NewMessage(pattern='offtopic|#ot'))
+async def handler(event):
+  if event.chat_id == -1001383980001 or event.chat_id == -1001365341799:
+    try:
+      ok = await event.get_reply_message()
+      oo = event.chat.username
+      pro = f"@{ok.sender.username} You are going offtopic\nyou got warn \nand also got banned \n\nofftopic [message](https://t.me/{event.chat.username}/{ok.id}) \nReporter : @{event.sender.username}"
+      await client.send_message(event.chat_id, pro)
+    except:
+      await event.reply("tag a message and type #ot or #offtopic")
+  else:
+     ok = await event.reply("this chat not made for this command")
+     await asyncio.sleep(8)
+     await ok.delete()
