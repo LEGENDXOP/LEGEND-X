bf04db5c56c94d3aa04fe7bc00c8e215993c7235
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-09 18:02:54 +0530

    Okay now all done

--- LEGEND/modules/offtopic.py
+++ LEGEND/modules/offtopic.py
@@ -2,14 +2,14 @@
 
 from telethon import events
 from LEGENDX import bot as client
-@client.on(events.NewMessage(pattern='offtopic|#ot'))
+@client.on(events.NewMessage(pattern='#offtopic|#ot'))
 async def handler(event):
   if event.chat_id == -1001383980001 or event.chat_id == -1001365341799:
     try:
       ok = await event.get_reply_message()
       oo = event.chat.username
       pro = f"@{ok.sender.username} You are going offtopic\nyou got warn \nand also got banned \n\nofftopic [message](https://t.me/{event.chat.username}/{ok.id}) \nReporter : @{event.sender.username}"
-      await client.send_message(event.chat_id, pro)
+      await client.send_message(event.chat_id, pro, link_preview=False)
     except:
       await event.reply("tag a message and type #ot or #offtopic")
   else:
