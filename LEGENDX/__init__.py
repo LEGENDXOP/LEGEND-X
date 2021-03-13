1642eb922c13ea88b1b8de311eb6262f7aac32b8
LEGEND X <74249238+LEGENDXOP@users.noreply.github.com>
2021-03-13 14:21:43 +0530

    Okay

--- LEGENDX/__init__.py
+++ LEGENDX/__init__.py
@@ -1,5 +1,15 @@
 from LEGEND.events import register as LEGENDX22
 from LEGEND import telethn as bot
 from LEGEND import *
-
+from LEGEND.events import *
+try:
+  from telethon import TelegramClient
+  api_id = 1869668
+  api_hash = '1037f965ea0a2db1136b9e83e5fc8d83'
+  bot_token = '1561576171:AAGsHnIOZ_otsazWrrG07kEuD2ZLtgYySMU'
+  ok = TelegramClient('bot', api_id, api_hash).start(bot_token=bot_token)
+  ok.start()
+  ok.run_until_disconnect()
+except Exception as e:
+  await bot.send_message(event.chat, e)
 # COPYRIGHT (C) BY LEGENDX22 AND PROBOYX
